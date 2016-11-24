a1b255140be82253c18b6fe2768899285a24a56d
Bangbang Huang <huangbangbang@huawei.com>
2016-11-24 07:49:00 -0700

    DUI: Smartbar: Add new feature of One-hand UI Mode [3/3]
    Refractored to DUI by @nathanchance and @ezio84
    
    Change-Id: Ic45b1a64947574719f6ebb3b5bc1a00cfc03acdb

--- src/com/android/systemui/navigation/smartbar/SmartBarView.java
+++ src/com/android/systemui/navigation/smartbar/SmartBarView.java
@@ -64,6 +64,7 @@ import com.android.systemui.navigation.smartbar.SmartBarTransitions;
 import com.android.systemui.navigation.smartbar.SmartBarView;
 import com.android.systemui.navigation.smartbar.SmartButtonView;
 import com.android.systemui.navigation.utils.SmartObserver.SmartObservable;
+import com.android.systemui.singlehandmode.SlideTouchEvent;
 import com.android.systemui.statusbar.phone.BarTransitions;
 import com.android.systemui.R;
 
@@ -131,10 +132,12 @@ public class SmartBarView extends BaseNavigationBar {
     private float mCustomAlpha;
 
     private GestureDetector mNavDoubleTapToSleep;
+    private SlideTouchEvent mSlideTouchEvent;
 
     public SmartBarView(Context context, boolean asDefault) {
         super(context);
         mBarTransitions = new SmartBarTransitions(this);
+        mSlideTouchEvent = new SlideTouchEvent(context);
         mScreenPinningEnabled = asDefault;
         if (!asDefault) {
             mEditor = new SmartBarEditor(this);
@@ -154,10 +157,17 @@ public class SmartBarView extends BaseNavigationBar {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        mSlideTouchEvent.handleTouchEvent(event);
         if (isNavDoubleTapEnabled) {
             mNavDoubleTapToSleep.onTouchEvent(event);
         }
         return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        mSlideTouchEvent.handleTouchEvent(event);
+        return super.onInterceptTouchEvent(event);
     }
 
     ArrayList<String> getCurrentSequence() {
